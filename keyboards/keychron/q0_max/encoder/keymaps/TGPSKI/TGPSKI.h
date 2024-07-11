e83ceabfc95f42ceca7d8421a4cd0e9a5ecb16cf
Tyler Pate <tpate@redhat.com>
2024-07-10 18:15:43 -0700

    first q0_max config

---
+++ keyboards/keychron/q0_max/encoder/keymaps/TGPSKI/TGPSKI.h
@@ -0,0 +1,24 @@
+/*
+Copyright 2024 Tyler Pate <tyler.graham.pate@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Enable layer indicators */
+#define BASE_LED_INDEX 0
+#define L1_LED_INDEX 1
+#define L2_LED_INDEX 2
+#define FN_LED_INDEX 3
