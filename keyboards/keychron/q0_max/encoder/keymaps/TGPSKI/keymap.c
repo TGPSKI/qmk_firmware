ca0e9bbb802307a9bb912bfc5fb2e02cc987d544
Tyler Pate <tpate@redhat.com>
2024-07-10 20:07:42 -0700

    layers working, no leds yet

--- keyboards/keychron/q0_max/encoder/keymaps/TGPSKI/keymap.c
+++ keyboards/keychron/q0_max/encoder/keymaps/TGPSKI/keymap.c
@@ -20,24 +20,24 @@
 
 enum layers {
     BASE,
-    FN,
+    L1,
     L2,
-    L3,
+    FN,
 };
 
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_tenkey_27(
-        KC_MUTE, KC_ESC, KC_DEL, KC_TAB, KC_BSPC,
+        KC_MUTE, _______, TO(L1), TO(L2), TO(FN),
         MC_1,	 KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
         MC_2,	 KC_P7,	 KC_P8,	 KC_P9,	 KC_PPLS,
         MC_3,	 KC_P4,	 KC_P5,	 KC_P6,
         MC_4,	 KC_P1,	 KC_P2,	 KC_P3,	 KC_PENT,
-        MO(FN),  KC_P0,          KC_PDOT         ),
+        MO(FN),    KC_P0,          KC_PDOT         ),
 
     [L1] = LAYOUT_tenkey_27(
-        _______, _______, _______, _______, _______,
+        _______, TO(BASE), _______, TO(L2), TO(FN),
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,          _______          ),
 
     [L2] = LAYOUT_tenkey_27(
-        _______, _______, _______, _______, _______,
+        _______, TO(BASE), TO(L1), _______, TO(FN),
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______,
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,          _______          ),
 
     [FN] = LAYOUT_tenkey_27(
-        RGB_TOG, BT_HST1, BT_HST2, BT_HST3, P2P4G,
-        _______, RGB_MOD, RGB_VAI, RGB_HUI, _______,
-        _______, RGB_RMOD,RGB_VAD, RGB_HUD, _______,
-        _______, RGB_SAI, RGB_SPI, KC_MPRV,
-        _______, RGB_SAD, RGB_SPD, KC_MPLY, _______,
+        RGB_TOG, TO(BASE), TO(L1), TO(L2), _______,
+        BT_HST1, RGB_MOD, RGB_VAI, RGB_HUI, _______,
+        BT_HST2, RGB_RMOD,RGB_VAD, RGB_HUD, _______,
+        BT_HST3, RGB_SAI, RGB_SPI, KC_MPRV,
+        P2P4G,   RGB_SAD, RGB_SPD, KC_MPLY, _______,
         _______, RGB_TOG,          KC_MNXT          )
 
 };
@@ -66,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [L1]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [L2]   = {ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
     [FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [L2]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [L3]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
 };
 #endif // ENCODER_MAP_ENABLE
 
@@ -77,4 +77,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     }
     return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case BASE:
+            rgb_matrix_set_color(BASE_LED_INDEX, RGB_RED);  // circle
+            rgb_matrix_set_color(L1_LED_INDEX, RGB_OFF);  // triangle
+            rgb_matrix_set_color(L2_LED_INDEX, RGB_OFF);  // square
+            rgb_matrix_set_color(FN_LED_INDEX, RGB_OFF);  // cross
+            break;
+        case L1:
+            rgb_matrix_set_color(BASE_LED_INDEX, RGB_OFF);  // circle
+            rgb_matrix_set_color(L1_LED_INDEX, RGB_RED);  // triangle
+            rgb_matrix_set_color(L2_LED_INDEX, RGB_OFF);  // square
+            rgb_matrix_set_color(FN_LED_INDEX, RGB_OFF);  // cross
+            break;
+        case L2:
+            rgb_matrix_set_color(BASE_LED_INDEX, RGB_OFF);  // circle
+            rgb_matrix_set_color(L1_LED_INDEX, RGB_OFF);  // triangle
+            rgb_matrix_set_color(L2_LED_INDEX, RGB_RED);  // square
+            rgb_matrix_set_color(FN_LED_INDEX, RGB_OFF);  // cross0
+            break;
+        case FN:
+            rgb_matrix_set_color(BASE_LED_INDEX, RGB_OFF);  // circle
+            rgb_matrix_set_color(L1_LED_INDEX, RGB_OFF);  // triangle
+            rgb_matrix_set_color(L2_LED_INDEX, RGB_OFF);  // square
+            rgb_matrix_set_color(FN_LED_INDEX, RGB_RED);  // cross 
+            break;
+    }
+    return state;
 }
